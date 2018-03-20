83f0bbde462f2c1d0a452428c64fe8ca39c36276
gendarius <dario.fac@gmail.com>
2018-03-20 22:36:40 +0100

    Added LMS
    Enjoy.

--- HW1.m
+++ HW1.m
@@ -100,3 +100,67 @@ plot(omega/(2*pi), 10*log10(s_white*abs(H).^2), 'Color', 'm');
 legend('Welch', 'Correlogram', 'Periodogram', ['AR(' int2str(N) ')'], 'Location', 'SouthWest')
 hold off
 title('Spectral analysis')
+
+
+[H, www] = freqz([1; a], 1, Nsamples, 'whole');
+figure('Name', 'Z-plane for error predictor A(z)');
+title('Z-plane for error predictor A(z)');
+zplane([1;a]);
+
+%% Least Mean Squares Filter
+
+% Length
+K = L;
+% Look up in the sky and set N
+N = 4;
+% Max number of iterations
+max_iter = 800;
+% Coefficients and error initialization
+c = zeros(N, max_iter + 1);
+e = zeros(1, max_iter);
+% Choice of parameter mu
+mu_tilde = 1;
+mu = mu_tilde/(rx(1)*N);
+% Center signal around its mean
+z = x - mean(x);
+
+for k = 1:max_iter
+    if (k < N + 1)
+        % Input vector of length N
+        z_k_1 = flipud([zeros(N - k + 1, 1); z(1:k - 1)]);
+        % For k = 1 z(1:0) is an empty matrix
+        y_k = z_k_1.'*c(:, k);
+    else
+        % Revert vector to obtain values from k-1 to k-N
+        z_k_1 = flipud(z((k - N):(k-1)));
+        y_k = z_k_1.'*c(:, k);
+    end
+    % d(k) is input signal z(k)
+    e_k = z(k) - y_k;
+    e(k) = e_k;
+    % Update step
+    c(:, k+1) = c(:, k) + mu*e_k*conj(z_k_1);
+end
+
+for index = 1:N
+    figure('Name', ['Coefficient of index ' int2str(index)]);
+    subplot(2, 1, 1)
+    plot(1:max_iter+1, real(c(index, :)));
+    hold on;
+    plot([1, max_iter+1], -real(a(index))*[1 1]);
+    title(['Real part of c' int2str(index) ' and c_{opt}' int2str(index)]);
+    legend(['c' int2str(index)], ['a' int2str(index)]);
+    xlim([0 max_iter]);
+    
+    subplot(2, 1, 2);
+    plot(1:max_iter+1, imag(c(index, :)));
+    hold on;
+    plot([1,max_iter+1], -imag(a(index))*[1 1]);
+    title(['Imaginary part of c' int2str(index) ' and c_{opt}' int2str(index)]);
+    legend(['c' int2str(index)], ['a' int2str(index)]);
+    xlim([0 max_iter]);
+end
+
+figure('Name', 'Error function');
+title('Error function at each iteration');
+plot(1:max_iter, 10*log10(abs(e).^2), 1:max_iter, 10*log10(s_white)*ones(1, max_iter));
